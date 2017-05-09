19d2aa0f6ea754eb8f8ad12a5a9c0a396a86b688
Mathias Forsberg <messias.forsberg@gmail.com>
2017-05-09 09:05:23 +0200

    Bootstrap og fonts

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Mammis</title>
+
+
+<!--Bootstrap og css -->
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:100');
+</style>
+
+
+<body>
+<h1>
+Hej jeg er en tekst der ikke står med Avenir fonten
+</h1>
+<h1 id="fonttest">
+Hej jeg er en tekst der bruger Avenir Fonten!
+</h1>
+<h1 id="fonttest2">
+Hej jeg er en tekst der bruger Roboto Fonten!
+</h1>
+</body>
+</html>
