6fff93f6ca562e16ede697bfbec78eed1fe5770e
Mforceberg <messias.forsberg@gmail.com>
2017-05-15 07:55:18 +0200

    forsøg på mute knap

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 include ("navigation.php");
 ?>
+
+
+
 <body>
 <div class="container-fluid full-width indexcontent">
       <div class="row">  
-        <video id="mammisvideo" class="col-lg-10 col-md-10" autoplay loop>
-        <source src="videos/mammismovie.mp4" type="video/mp4">
+        <video id="mammisvideo" class="col-lg-10 col-md-10" autoplay loop controls>
+        <source src="videos/mammismovieworking.mp4" type="video/mp4" id="myVideo">
         </video>
+        
+        <button class="mute-video">toggle</button>  
+          
         <img src="images/aabningstider.jpg" alt="mammis gelatos åbningstider" class="col-lg-2 col-md-2 img-fluid" id="mammisaabning">
     </div>
     <div class="row">
