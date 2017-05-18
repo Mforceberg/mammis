4dec5175e840737b3fa9c64270fcc5b1538a862c
Mforceberg <messias.forsberg@gmail.com>
2017-05-18 21:02:39 +0200

    Produkter

--- produkter.php
+++ produkter.php
@@ -4,7 +4,10 @@ include ("navigation.php");
 
 <div class="full-width">
     <div class="row">
-        
-        <img src="images/priser.jpg" alt="Tavle med priser og produkter" class="colsm-12 col-md-12 col-lg-12">
+        <img src="images/priser.jpg" alt="Tavle med priser og produkter" class="col-sm-12 col-md-12 col-lg-12" id="produktTavle">
+    </div>
+    <div class="row">
+        <img src="images/stortbilledeprodukter.jpg" alt="Billede af en is med chokolade og pistacie" class="col-sm-5 col-md-5 col-lg-5" id="stortProdukt">
+        <img src="images/produkterhoejre.jpg" alt="Forskellige produkter som sælges ved Mammis Gelato" class="col-sm-7 col-md-7 col-lg-7" id="smaaProdukter">
     </div>
 </div>
