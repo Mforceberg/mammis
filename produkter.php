d3a6a3a583e841f2bb751885e279478ac8cb67e8
Mathias Forsberg <messias.forsberg@gmail.com>
2017-05-18 14:58:58 +0200

    Start på produkter

--- produkter.php
+++ produkter.php
@@ -2,3 +2,9 @@
 include ("navigation.php");
 ?>
 
+<div class="full-width">
+    <div class="row">
+        
+        <img src="images/priser.jpg" alt="Tavle med priser og produkter" class="colsm-12 col-md-12 col-lg-12">
+    </div>
+</div>
